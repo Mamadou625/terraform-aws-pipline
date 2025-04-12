4c9dfbd65bc63cfbf911e9576676a84f0f13c08f
Mamadou <mdian3998@gmail.com>
2025-04-12 12:04:59 -0400

    deletion of EC2

--- main.tf
+++ main.tf
@@ -12,12 +12,3 @@ terraform {
 provider "aws" {
   region  = "ca-central-1"
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-02cd5b9bfb2512340"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "BanoServer"
-  }
-}

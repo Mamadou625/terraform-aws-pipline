e75916776341bcf7f6a3595aec6a172ecf429e5a
Mamadou <mdian3998@gmail.com>
2025-04-12 11:00:01 -0400

    creation of an EC2

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ca-entral-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-02cd5b9bfb2512340"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MamadouServer"
+  }
+}
